f3bfe3f64eac69e8071e1b34817332bb499b6bfa
iayushanand <clashofserver8823@gmail.com>
2026-07-26 02:33:35 +0530

    nativewind setup

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+import "./global.css";
 
-import './global.css';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+
+import GetStarted from "components/GetStarted";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      <GetStarted />
+      <StatusBar style="dark" />
     </SafeAreaProvider>
   );
 }
---
+++ components/GetStarted.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function GetStarted() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-3xl font-bold text-blue-500">
+        Welcome to Recsta!
+      </Text>
+    </View>
+  );
+}
